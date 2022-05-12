fd0ab99f3429c1e9f397300003aca92972844678
Tooonia <tooonia@gmail.com>
2022-05-12 13:24:12 +0200

    DTO potential conception error (Front CREATE affected?) #2
    Small comment on PixelArtDto with @JsonProperty annotation use

--- src/main/java/com/simplon/pixelartback/storage/dto/pixelart/PixelArtDto.java
+++ src/main/java/com/simplon/pixelartback/storage/dto/pixelart/PixelArtDto.java
@@ -14,6 +14,8 @@ public class PixelArtDto {
 //    TODO private or public properties here? In MapStruct, DTO with public properties and no getters/setters?
 
 //    TODO: is this necessary here? Nem kellene valamely ertekeket elrejteni?
+//    TODO: Mustapha: probablement cela indique que ce sera dans le JSON body!!!
+//    TODO: Il récupère une propriété du Body, alors que nous voulons une partie de l'URL (l'id qui se figure dans le URL!!!
     @JsonProperty("id")
     private Long id;
 //    @JsonProperty("uuid")
